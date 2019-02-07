392b1aa727552bee6171c8bf0b95bfda22900e08
Nicola Corti <nco@yelp.com>
2019-02-07 17:48:59 +0100

    Add an example of generated code

---
+++ sample-generated-code/src/main/java/com/yelp/codegen/generatecodesamples/tools/XNullable.kt
@@ -0,0 +1,13 @@
+package com.yelp.codegen.generatecodesamples.tools
+
+import com.squareup.moshi.JsonQualifier
+
+/**
+ * Marks an optional variable that will be forcibly serialized as "null" when being transformed
+ * to json. (By default, an optional variable is omitted from the output json string,
+ * for optimisation purposes.)
+ */
+@Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+@JsonQualifier
+annotation class XNullable
