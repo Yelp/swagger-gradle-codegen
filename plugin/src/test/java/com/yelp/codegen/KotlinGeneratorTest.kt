816b48aab9cccf3be2831a6cdf7f9d51767fd9ce
Samuele Maci <macisamuele@gmail.com>
2019-02-20 16:05:15 +0100

    Update test naming to follow typical JUnit test naming convention

--- plugin/src/test/java/com/yelp/codegen/KotlinGeneratorTest.kt
+++ plugin/src/test/java/com/yelp/codegen/KotlinGeneratorTest.kt
@@ -5,11 +5,10 @@ import org.junit.Test
 class KotlinGeneratorTest {
 
     @Test
-    fun test_toModelName_does_not_trim_too_much() {
+    fun toModelName_doesNotTrimTooMuch() {
         assert(KotlinGenerator().toModelName("model") == "Model")
         assert(KotlinGenerator().toModelName("model with space") == "ModelWithSpace")
         assert(KotlinGenerator().toModelName("model with dot.s") == "ModelWithDotS")
         assert(KotlinGenerator().toModelName("model with userscore_s") == "ModelWithUserscoreS")
     }
-
 }
