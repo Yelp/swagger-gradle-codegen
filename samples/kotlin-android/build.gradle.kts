d0811d62c2722657245c3c0a028f745beffd439a
Nicola Corti <corti.nico@gmail.com>
2019-05-20 17:52:02 +0200

    Updated Gradle dependencies to have the RxJava2 Adapters

--- samples/kotlin-android/build.gradle.kts
+++ samples/kotlin-android/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("com.squareup.moshi:moshi-adapters:1.8.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.8.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.5.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.5.0")
 
     // Date Support
     implementation("com.jakewharton.threetenabp:threetenabp:1.1.1")
