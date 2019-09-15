d0b07b7d2ab2cf0f4b44cdb99a3d67cccfdb76b9
Nicola Corti <corti.nico@gmail.com>
2019-09-15 22:02:04 +0200

    Update multiple dependencies
    Gradle to 5.6
    AGP to 3.5.0
    Kotlin to 1.3.50
    Retrofit to 2.6.1
    RxJava2 to 2.2.11
    Detekt to 1.0.1

--- samples/kotlin-android/build.gradle.kts
+++ samples/kotlin-android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.library") version "3.4.2"
-    kotlin("android") version "1.3.41"
+    id("com.android.library") version "3.5.0"
+    kotlin("android") version "1.3.50"
     id("com.yelp.codegen.plugin") version "1.2.0"
 }
 
@@ -16,23 +16,23 @@ android {
 
 dependencies {
     // Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.41")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.41")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.50")
 
     // Moshi + OkHttp + Retrofit
     implementation("com.squareup.moshi:moshi:1.8.0")
     implementation("com.squareup.moshi:moshi-adapters:1.8.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.8.0")
     implementation("com.squareup.okhttp3:okhttp:3.12.3")
-    implementation("com.squareup.retrofit2:retrofit:2.6.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.6.0")
-    implementation("com.squareup.retrofit2:adapter-rxjava2:2.6.0")
+    implementation("com.squareup.retrofit2:retrofit:2.6.1")
+    implementation("com.squareup.retrofit2:converter-moshi:2.6.1")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.6.1")
 
     // Date Support
     implementation("com.jakewharton.threetenabp:threetenabp:1.2.1")
 
     // RxJava
-    implementation("io.reactivex.rxjava2:rxjava:2.2.10")
+    implementation("io.reactivex.rxjava2:rxjava:2.2.11")
     implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
 }
 
