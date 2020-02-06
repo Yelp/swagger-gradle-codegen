8fadb8607d0c2d5d23e56a2909116d30a1a75d63
Martin Bonnin <martin@mbonnin.net>
2020-02-06 18:11:25 +0100

    remove unneeded android things

--- gradle-plugin/settings.gradle.kts
+++ gradle-plugin/settings.gradle.kts
@@ -3,14 +3,6 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
         jcenter()
-        google()
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if ("com.android" in requested.id.id) {
-                useModule("com.android.tools.build:gradle:${requested.version}")
-            }
-        }
     }
 }
 
