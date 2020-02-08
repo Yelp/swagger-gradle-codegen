2af3debdd4b416b1135276a8babb7177f8efa355
Martin Bonnin <martin@mbonnin.net>
2020-02-08 13:02:17 +0100

    actually, this needs to be annotated as well

--- gradle-plugin/plugin/src/main/java/com/yelp/codegen/plugin/FeatureConfiguration.kt
+++ gradle-plugin/plugin/src/main/java/com/yelp/codegen/plugin/FeatureConfiguration.kt
@@ -1,7 +1,11 @@
 package com.yelp.codegen.plugin
 
 import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 
 class FeatureConfiguration(objectFactory: ObjectFactory) {
+    @get:Input
+    @get:Optional
     val headersToRemove = objectFactory.listProperty(String::class.java)
 }
