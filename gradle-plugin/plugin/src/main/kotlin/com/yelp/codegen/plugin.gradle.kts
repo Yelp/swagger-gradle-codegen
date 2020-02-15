06d1976e8cc6d24b6e8c9e0bd4af6de7133b226b
Martin Bonnin <martin@mbonnin.net>
2020-02-15 20:46:44 +0100

    add a test against an older version of the Gradle Plugin

--- gradle-plugin/plugin/src/main/kotlin/com/yelp/codegen/plugin.gradle.kts
+++ gradle-plugin/plugin/src/main/kotlin/com/yelp/codegen/plugin.gradle.kts
@@ -2,8 +2,13 @@ package com.yelp.codegen
 
 import com.yelp.codegen.plugin.GenerateTask
 import com.yelp.codegen.plugin.GenerateTaskConfiguration
+import org.gradle.util.GradleVersion
 
 val config = extensions.create("generateSwagger", GenerateTaskConfiguration::class.java, project)
+
+require(GradleVersion.current().compareTo(GradleVersion.version("5.0")) >= 0) {
+    "com.yelp.codegen.plugin requires Gradle version 5.0 or greater"
+}
 
 tasks {
     register<GenerateTask>("generateSwagger") {
