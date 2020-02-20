97420073f506e4aae58ba3394844f3d0fe5ef43b
Martin Bonnin <martin@mbonnin.net>
2020-02-20 23:43:53 +0100

    keep detekt happy

--- gradle-plugin/plugin/src/main/java/com/yelp/codegen/plugin/GenerateTaskConfiguration.kt
+++ gradle-plugin/plugin/src/main/java/com/yelp/codegen/plugin/GenerateTaskConfiguration.kt
@@ -5,6 +5,7 @@ import java.io.File
 import javax.inject.Inject
 import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.file.RegularFileProperty
 
 abstract class GenerateTaskConfiguration @Inject constructor(project: Project) {
     val objects = project.objects
@@ -12,10 +13,10 @@ abstract class GenerateTaskConfiguration @Inject constructor(project: Project) {
     val platform = objects.property(String::class.java).convention("kotlin")
     val packageName = objects.property(String::class.java).convention("com.codegen.default")
     val specName = objects.property(String::class.java).convention("defaultname")
+    abstract val inputFile: RegularFileProperty
     val specVersion = objects.property(String::class.java).convention(project.provider {
         readVersionFromSpecfile(inputFile.get().asFile)
     })
-    val inputFile = objects.fileProperty()
     val outputDir = objects.directoryProperty().convention(project.layout.buildDirectory.dir(DEFAULT_OUTPUT_DIR).get())
 
     val extraFiles = objects.directoryProperty()
